9915fb97a63079b76878da9d272e576a0e046dca
Michael Steve <themichaelsteve@gmail.com>
2024-07-18 14:51:53 +0100

    local storage update

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
-document.addEventListener('DOMContentLoaded', addTask);
-
+document.addEventListener('DOMContentLoaded', () => {
   const addButton = document.getElementById('add-task-btn');
   const taskInput = document.getElementById('task-input');
   const taskList = document.getElementById('task-list');
 
-  function addTask() {
-    let taskText = taskInput.value.trim();
+  loadTasks();
+
+  function loadTasks() {
+    const storedTasks = JSON.parse(localStorage.getItem('tasks') || '[]');
+    storedTasks.forEach(taskText => {
+      console.log(taskText);
+      const list = document.createElement('li');
+      list.textContent = taskText;
+      const newBtn = document.createElement('button');
+      newBtn.textContent = 'Remove';
+      newBtn.classList.add('remove-btn');
+      newBtn.addEventListener('click', () => {
+      
+        list.remove();
+      });
+      list.appendChild(newBtn);
+      taskList.appendChild(list);
+
+      taskInput.value = '';
+    });
+   
+};
+
+  function addTask(taskText, save = true) {
+   taskText = taskInput.value.trim();
     if (taskText === '') {
       alert("Please enter a task");
       return;
@@ -24,6 +46,11 @@ document.addEventListener('DOMContentLoaded', addTask);
 
       taskInput.value = '';
     }
+    if (save) {
+      storedTasks = JSON.parse(localStorage.getItem('tasks') || '[]');
+      storedTasks.push(taskText);
+      localStorage.setItem('tasks', JSON.stringify(storedTasks));
+  }
   }
 
   addButton.addEventListener('click', addTask);
@@ -34,5 +61,14 @@ document.addEventListener('DOMContentLoaded', addTask);
       addTask();
     } 
   });
+});
+
+  
+
+ 
+
+  
+
+ 
 
 
