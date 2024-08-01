09d8d70aadab6e390aab5597434470c812b29112
Michael <themichaelsteve@gmail.com>
2024-08-01 16:27:38 +0100

    Project Completed

--- script.js
+++ script.js
@@ -3,55 +3,75 @@ document.addEventListener('DOMContentLoaded', () => {
   const taskInput = document.getElementById('task-input');
   const taskList = document.getElementById('task-list');
 
+  let tasks = [];
   loadTasks();
 
   function loadTasks() {
-    const storedTasks = JSON.parse(localStorage.getItem('tasks') || '[]');
-    storedTasks.forEach(taskText => {
-      console.log(taskText);
-      const list = document.createElement('li');
-      list.textContent = taskText;
-      const newBtn = document.createElement('button');
-      newBtn.textContent = 'Remove';
-      newBtn.classList.add('remove-btn');
-      newBtn.addEventListener('click', () => {
-      
-        list.remove();
+    const storedTasks = JSON.parse(localStorage.getItem('tasks'));
+    if(storedTasks == null){
+      return;
+    } 
+
+      storedTasks.map(taskText => {
+        const list = document.createElement('li');
+        list.textContent = taskText;
+        const newBtn = document.createElement('button');
+        newBtn.textContent = 'Remove';
+        newBtn.classList.add('remove-btn');
+        list.appendChild(newBtn);
+        taskList.appendChild(list);
+  
+       // Removing a task
+       newBtn.addEventListener("click", () => {
+        list.remove(); // Remove it from DOM
+        
+        // Remove item from array
+        index = storedTasks.indexOf(taskText);
+        storedTasks.splice(index, 1);
+    
+        localStorage.setItem("tasks", JSON.stringify(storedTasks));
+    
+        console.log(storedTasks);
       });
-      list.appendChild(newBtn);
-      taskList.appendChild(list);
+ });
+}
 
-      taskInput.value = '';
-    });
-   
-};
-
-  function addTask(taskText, save = true) {
+  function addTask(taskText) {
    taskText = taskInput.value.trim();
     if (taskText === '') {
       alert("Please enter a task");
       return;
-    } else if(taskText !== '') {
+    } else {
       const list = document.createElement('li');
       list.textContent = taskText;
       const newBtn = document.createElement('button');
       newBtn.textContent = 'Remove';
       newBtn.classList.add('remove-btn');
-
-      newBtn.addEventListener('click', () => {
-        list.remove();
-      });
       list.appendChild(newBtn);
       taskList.appendChild(list);
 
+      // push new task to task array
+      tasks.push(taskText);
+
+      //save in local storage
+      storedTasks = localStorage.setItem('tasks', JSON.stringify(tasks));
+
+      // Resets the input field
       taskInput.value = '';
+
+      // Removing a task
+       newBtn.addEventListener("click", () => {
+        list.remove(); // Remove it from DOM
+        
+        // Remove it from tasks array
+        index = tasks.indexOf(taskText);
+        tasks.splice(index, 1);
+    
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+    
+      });
     }
-    if (save) {
-      storedTasks = JSON.parse(localStorage.getItem('tasks') || '[]');
-      storedTasks.push(taskText);
-      localStorage.setItem('tasks', JSON.stringify(storedTasks));
-  }
-  }
+  };
 
   addButton.addEventListener('click', addTask);
 
@@ -62,8 +82,6 @@ document.addEventListener('DOMContentLoaded', () => {
     } 
   });
 });
-
-  
 
  
 
